746bce8167cb7052c00892fcc2b66350a252ea6a
lucasrbsouza <joselucastrbssilvasouza@gmail.com>
2025-05-06 16:22:54 -0300

    estudos de angular diretiva, templates e decorators dinamicos

--- 1.componentes/diretivas_templates_e_decorators_dinamicos/src/app/app.component.ts
+++ 1.componentes/diretivas_templates_e_decorators_dinamicos/src/app/app.component.ts
@@ -1,20 +1,42 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FilhoComponent } from './filho/filho.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
+
   clicou(input: HTMLInputElement){
     alert(input.value)
   }
 
-  @ViewChild('input_meu')
+  @ViewChild('input_meu',)
   meuInputEl!: ElementRef<HTMLInputElement>;
 
   udpateInputText(){
     this.meuInputEl.nativeElement.value = 'Texto Ataualizado'
+  }
+
+  @ViewChild('filhoComp')
+  filhoCompRef!: FilhoComponent
+
+  hello(){
+    this.filhoCompRef.dizerOi()
+    this.filhoCompRef.message = "oii"
+  }
+
+  constructor(){
+
+  }
+ 
+  ngOnInit(): void {
+    console.log(this.meuInputEl);
+    
+  }
+  ngAfterViewInit(): void {
+    this.meuInputEl.nativeElement.focus()
   }
 }
 
